d8598eb541849f55bda79eabcdb7ffd9da08fbc8
Gcv Kishore <gcvkishore@msitprogram.net>
2022-09-19 16:17:10 +0530

    updated with cors

--- handler.js
+++ handler.js
@@ -2,7 +2,14 @@
 const mongoose = require("mongoose");
 const Registration = require("./registration");
 
+const connectDB = () => {
+  const dbUrl = "mongodb+srv://gcvkishore:YQ0c1dwxZmGA70IN@cluster0.a1c4u.mongodb.net/?retryWrites=true&w=majority";
+  mongoose.connect(dbUrl).then();
+  return mongoose.connection;
+}
+
 module.exports.getData = async (event) => {
+  
   const con = await connectDB();
   const find_data = await Registration.find();
   return {
@@ -10,29 +17,41 @@ module.exports.getData = async (event) => {
     body: JSON.stringify(
      find_data
     ),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT, DELETE ",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "*"
+  },
   };
 };
 
 module.exports.postData = async (event) => {
   const con = await connectDB();
-  const save_data = await Registration.save();
+  const body = JSON.parse(event.body);
+  
   const registration = new Registration({
     firstName: body.firstName,
     lastName: body.lastName,
     emailID: body.emailID,
     phoneNumber: body.phoneNumber,
     dailCode: body.dailCode,
-});
+  });
+  const save_data = await registration.save();
   return {
     statusCode: 200,
     body: JSON.stringify(
-     save_data
+    save_data
     ),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT, DELETE ",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "*"
+  },
   };
 };
-
-const connectDB = () => {
-  const dbUrl = "mongodb+srv://gunti:JPJCF7PhIiWI9jKu.xfw8mws.mongodb.net/?retryWrites=true&w=majority";
-  mongoose.connect(dbUrl);
-  return mongoose.connection;
-}
